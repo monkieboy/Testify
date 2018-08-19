f83dbbeef9b8bfc67cfca27f3183c97f12f1aa87
Mark Gray <mgray@globalreach-partners.com>
2018-08-19 19:21:15 +0100

    Updating License info link

--- pack.cmd
+++ pack.cmd
@@ -1,4 +1,8 @@
-SET NUGETKEY=%1
-SET NUGETVERSION=%2
-dotnet pack Bdd -c Release /p:PackageVersion=%NUGETVERSION %
-dotnet nuget push Bdd/bin/Release/Testify.Bdd.%NUGETVERSION %.nupkg -k %NUGETKEY % -s nuget.org
+SET "NUGETVERSION=%1"
+SET "NUGETKEY=%2"
+SET "PACKACTION=Bdd -c Release /p:PackageVersion=%NUGETVERSION%"
+SET "PUSHACTION=Bdd/bin/Release/Testify.Bdd.%NUGETVERSION%.nupkg -k %NUGETKEY% -s nuget.org"
+ECHO %PACKACTION%
+ECHO %PUSHACTION%
+dotnet pack %PACKACTION%
+dotnet nuget push %PUSHACTION%
