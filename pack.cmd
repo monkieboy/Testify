091b010d34c054c18661ae419ecafa8b729fd5b0
Mark Gray <mgray@globalreach-partners.com>
2018-08-19 19:01:02 +0100

    Setting the packs scripts

--- pack.cmd
+++ pack.cmd
@@ -1,3 +1,4 @@
-NUGETVERSION=1.0.0
-dotnet pack Bdd -c Release /p:PackageVersion=$NUGETVERSION
-dotnet nuget push Bdd/bin/Release/Testify.Bdd.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
+SET NUGETKEY=%1
+SET NUGETVERSION=%2
+dotnet pack Bdd -c Release /p:PackageVersion=%NUGETVERSION %
+dotnet nuget push Bdd/bin/Release/Testify.Bdd.%NUGETVERSION %.nupkg -k %NUGETKEY % -s nuget.org
